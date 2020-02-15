6b060bc272e8f81300f02bd92ee5f9ae5dceeaf0
Gabriel Sha <gabrielshachen@gmail.com>
2020-02-15 12:06:43 +0800

    Build search prototype

--- theModernJavascriptBootcamp/applicationDesignPatterns/index.js
+++ theModernJavascriptBootcamp/applicationDesignPatterns/index.js
@@ -3,15 +3,30 @@
 // 2. Identify challenging aspects of the project we're about to work on
 // 3. Get started to the project and write a lot of codes
 
-const fetchData = async () => {
+const fetchData = async (searchTerm) => {
 	const response = await axios.get('http://www.omdbapi.com/', {
+		// axios function; or use http://www.omdbaqi.com/?d9835cc5
 		params: {
 			apikey: 'd9835cc5',
-			s: 'avengers'
+			s: searchTerm
 		}
 	});
 
 	console.log(response.data);
 };
 
-fetchData();
+const input = document.querySelector('input');
+
+// start search after 1 second pause
+let timeoutId;
+const onInput = (event) => {
+	if (timeoutId) {
+		// every input, the timeout restarts
+		clearTimeout(timeoutId);
+	}
+	timeoutId = setTimeout(() => {
+		fetchData(event.target.value);
+	}, 1000);
+};
+
+input.addEventListener('keypress', onInput);
